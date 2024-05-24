28de140012c92500aad4f7ce993c4801480b5920
Sebastian <jsga2505@gmail.com>
2024-05-23 19:41:38 -0500

    add experience/payment domains

---
+++ src/app/domains/experience-page/experience-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './experience-page.component.html',
+  styleUrl: './experience-page.component.css'
+})
+export class ExperiencePageComponent {
+
+}
