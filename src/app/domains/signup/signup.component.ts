b276e66d4a5f41f42b73a4d7f18e88c3af34a1a1
Nelson <lvnelson.via@gmail.com>
2024-05-29 20:12:10 -0600

    Add html / css sign up files

--- src/app/domains/signup/signup.component.ts
+++ src/app/domains/signup/signup.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
