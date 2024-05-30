b57343fae4082a7a99acdffa79228ef5bfed279f
Juanes <juanespachon1203@gmail.com>
2024-05-29 20:52:56 -0500

    Add header Component

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
