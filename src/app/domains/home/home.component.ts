98311de2656191968c4a80ea2da0699c165c8174
Juanes <juanespachon1203@gmail.com>
2024-05-31 14:00:56 -0500

    Add Home Domain Content & Install Normalize.css

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { ExperienceComponent } from '../../components/experience/experience.component';
+import { MobileTopExperienceComponent } from '../../components/mobile-top-experience/mobile-top-experience.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent, ExperienceComponent, MobileTopExperienceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  //Js Logic of the Html
+
+  ngAfterViewInit(): void {
+    window.addEventListener('scroll', this.onScroll);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.onScroll);
+  }
+
+  private onScroll = (): void => {
+    let progressBar = document.getElementById('progress__bar');
+    if (progressBar) {
+      let scrollTop = window.scrollY || document.documentElement.scrollTop;
+      let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      let progress = (scrollTop / scrollHeight) * 100;
+      progressBar.style.width = progress + '%';
+    }
+  }
 
 }
