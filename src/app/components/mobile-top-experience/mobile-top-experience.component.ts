e66c335e61043e47809d9cb79821f49c97e1ff21
Juanes <juanespachon1203@gmail.com>
2024-05-31 12:46:29 -0500

    Create All Home components

---
+++ src/app/components/mobile-top-experience/mobile-top-experience.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-top-experience',
+  standalone: true,
+  imports: [],
+  templateUrl: './mobile-top-experience.component.html',
+  styleUrl: './mobile-top-experience.component.css'
+})
+export class MobileTopExperienceComponent {
+
+}
