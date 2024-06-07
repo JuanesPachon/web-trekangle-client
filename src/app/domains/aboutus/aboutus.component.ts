4aef710ad5ab00428706d8b60b796ddeeed30c70
Nelson <lvnelson.via@gmail.com>
2024-06-07 16:37:32 -0600

    Edit about us domain

--- src/app/domains/aboutus/aboutus.component.ts
+++ src/app/domains/aboutus/aboutus.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+
 
 @Component({
   selector: 'app-aboutus',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './aboutus.component.html',
   styleUrl: './aboutus.component.css'
 })
