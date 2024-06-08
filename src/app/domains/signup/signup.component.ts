ecabf256a145a2508b0855e0efa55e09aa35a455
Nelson <lvnelson.via@gmail.com>
2024-06-07 20:27:15 -0600

    Add router links

--- src/app/domains/signup/signup.component.ts
+++ src/app/domains/signup/signup.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { RouterLink, RouterLinkWithHref } from '@angular/router';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,FooterComponent,RouterLinkWithHref],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
