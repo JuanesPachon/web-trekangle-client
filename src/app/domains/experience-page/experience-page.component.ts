7b55b50f05e1ca40ea0934c881c9aee089897597
Sebastian <jsga2505@gmail.com>
2024-06-11 17:11:07 -0500

    edit login and detail domains

--- src/app/domains/experience-page/experience-page.component.ts
+++ src/app/domains/experience-page/experience-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
 import { RouterLink, RouterLinkWithHref } from '@angular/router';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-experience-page',
   standalone: true,
-  imports: [RouterLinkWithHref],
+  imports: [HeaderComponent,FooterComponent,RouterLinkWithHref],
   templateUrl: './experience-page.component.html',
   styleUrl: './experience-page.component.css'
 })
--- src/app/domains/login/login.component.ts
+++ src/app/domains/login/login.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
 import { RouterLinkWithHref } from '@angular/router';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLinkWithHref],
+  imports: [HeaderComponent,FooterComponent,RouterLinkWithHref],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
