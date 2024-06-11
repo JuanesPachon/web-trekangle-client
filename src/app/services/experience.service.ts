7f48c8310fb9423ef314ddd150934b214ffed03d
Juanes <juanespachon1203@gmail.com>
2024-06-11 17:24:33 -0500

    Add Services fold and import httpClient

---
+++ src/app/services/experience.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExperienceService {
+
+  private http = inject(HttpClient);
+
+  listExperiences(page: number, limit?: number): Observable<any> {
+    const queryParams = `?page=${page}${limit ? `&limit=${limit}` : ''}`;
+    return this.http.get(`http://localhost:3000/experiences/${queryParams}`);
+  }
+
+}
