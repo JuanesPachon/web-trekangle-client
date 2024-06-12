4cefbe56e643171a5925984621ecc61c556d6fbc
Juanes <juanespachon1203@gmail.com>
2024-06-12 16:59:24 -0500

    Add functionality to User-button in Header component & Edit Experience card in Home domain

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, signal, HostListener } from '@angular/core';
 import { RouterLinkWithHref } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLinkWithHref],
+  imports: [RouterLinkWithHref, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  private Router = inject(Router);
+  private userService = inject(UserService);
+  private authService = inject(AuthService);
+  private elementRef = inject(ElementRef);
+
+  isloggedIn() {
+    return this.userService.isLoggedIn();
+  }
+
+  logOut() {
+    return this.authService.removeToken();
+  }
+
+  dropdown = signal(false);
+
+  openDropdown() {
+    this.dropdown.update((value) => !value);
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      if (this.dropdown()) {
+        this.dropdown.set(false);
+      }
+    }
+
+  }
 }
