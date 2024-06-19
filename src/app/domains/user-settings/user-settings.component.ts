4953bcc1646db854040bea91b63257fbadeb58e1
Juanes <juanespachon1203@gmail.com>
2024-06-18 23:25:59 -0500

    Add Edit User request & and other details

--- src/app/domains/user-settings/user-settings.component.ts
+++ src/app/domains/user-settings/user-settings.component.ts
@@ -5,18 +5,26 @@ import { CommonModule } from '@angular/common';
 import { BookingCardComponent } from '../../components/booking-card/booking-card.component';
 import { CartExperienceComponent } from '../../components/cart-experience/cart-experience.component';
 import { NotificationService } from '../../services/notification.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-settings',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, BookingCardComponent, CommonModule, CartExperienceComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    BookingCardComponent,
+    CommonModule,
+    CartExperienceComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './user-settings.component.html',
   styleUrl: './user-settings.component.css',
 })
 export class UserSettingsComponent {
-
   private notificationService = inject(NotificationService);
-
+  private userService = inject(UserService);
 
   // Edit Section
 
@@ -46,6 +54,39 @@ export class UserSettingsComponent {
     this.editPassword.update((value) => !value);
   }
 
+  userSettingsForm = new FormGroup({
+    name: new FormControl(''),
+    userName: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  errorMessage: string = '';
+  successMessage = signal(false);
+  activeEdition = signal(false);
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    this.userService.editUser(this.userSettingsForm.value).subscribe({
+      next: (response) => {
+        console.log(response);
+        localStorage.setItem('activeEdition', 'true');
+        window.location.reload();
+      },
+      error: (error) => {
+        if (error.status === 409) {
+          this.errorMessage = 'Email is already in use';
+        } else if (error.status === 400) {
+          this.errorMessage = error.error.errors[0];
+        } else {
+          this.errorMessage =
+            'An unexpected error occurred. Please try again later.';
+        }
+        console.log(error);
+      },
+    });
+  }
+
   //Toggle Section
 
   userBookings = signal<boolean | null>(null);
@@ -68,12 +109,28 @@ export class UserSettingsComponent {
     }
   }
 
+  user = signal<any>({});
+
   ngOnInit() {
     this.onResize({ target: window });
+
+    this.userService.getUser().subscribe({
+      next: (user) => {
+        this.user.set(user);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+
+    if (localStorage.getItem('activeEdition') === 'true') {
+      this.activeEdition.set(true);
+      localStorage.removeItem('activeEdition');
+    }
+
   }
 
   //notification logic
 
   activeNotification = this.notificationService.showBookingNotification;
-  
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {jwtDecode} from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root',
@@ -21,6 +22,34 @@ export class UserService {
       email: formValues.email,
       password: formValues.password,
     });
+  }
+
+  editUser(formValues: any) {
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + localStorage.getItem('user_token')
+    });
+
+    const token: any = localStorage.getItem('user_token');
+    const decodedToken: any = jwtDecode(token);
+
+    const body: any = {};
+    for (const key in formValues) {
+      if (formValues.hasOwnProperty(key) && formValues[key] !== undefined && formValues[key] !== null && formValues[key] !== '') {
+        body[key] = formValues[key];
+      }
+    }
+
+    return this.http.patch('http://localhost:3000/users/' + decodedToken.sub, body, { headers: headers });
+  }
+
+  
+
+  getUser() {
+    const token: any = localStorage.getItem('user_token');
+    const decodedToken: any = jwtDecode(token);
+    return this.http.get(`http://localhost:3000/users/` + decodedToken.sub);
   }
 
   
